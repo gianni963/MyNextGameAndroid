099a4f18dbb5132ad0bc3467661399d3ebd9ab0b
gianni963 <gianni_guatieri@hotmail.com>
2021-08-19 05:38:00 -0700

    fix divers

---
+++ app/src/main/java/be/bxl/formation/mynextgame/adapter/MvoieDetailsAdapter.kt
@@ -0,0 +1,10 @@
+package be.bxl.formation.mynextgame.adapter
+
+import be.bxl.formation.mynextgame.models.Game
+
+class MvoieDetailsAdapter(
+    private val layoutId: Int,
+    private val game : Game
+) {
+
+}
