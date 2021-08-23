941dfd0c991f2e608d55d8d601b3da28a09f8e9f
gianni963 <gianni_guatieri@hotmail.com>
2021-08-23 07:02:17 -0700

    add favourite game adapter, need to fix the display in FragmentWishList

---
+++ app/src/main/java/be/bxl/formation/mynextgame/adapter/FavouriteGameAdapter.kt
@@ -0,0 +1,55 @@
+package be.bxl.formation.mynextgame.adapter
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ListAdapter
+import android.widget.ListView
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import be.bxl.formation.mynextgame.MainActivity
+import be.bxl.formation.mynextgame.MoviePopup
+import be.bxl.formation.mynextgame.RegisterUser
+import be.bxl.formation.mynextgame.models.Game
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.item_horizontal_game.view.*
+import kotlinx.android.synthetic.main.item_vertical_game.view.*
+import kotlinx.android.synthetic.main.item_vertical_game.view.image_item
+
+class FavouriteGameAdapter(
+    private val layoutId: Int,
+    private val games : List<Game>,
+) : RecyclerView.Adapter<FavouriteGameAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val IMAGE_BASE = "https://image.tmdb.org/t/p/w500/"
+
+
+        fun bindGame(game: Game) {
+            val w1 = game
+            println("!!---------- !!! we are looking for $w1")
+            itemView.name_popular_item.text = game.title
+            itemView.platform_popular_item.text = game.release
+            itemView.id_popular_item.text = game.id
+            Glide.with(itemView).load(IMAGE_BASE + game.poster).into(itemView.image_item)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(layoutId, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindGame(games[position])
+
+    }
+    override fun getItemCount(): Int = 10
+}
--- app/src/main/java/be/bxl/formation/mynextgame/fragments/FragmentWishList.kt
+++ app/src/main/java/be/bxl/formation/mynextgame/fragments/FragmentWishList.kt
@@ -10,32 +10,16 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import be.bxl.formation.mynextgame.Db.GameDao
 import be.bxl.formation.mynextgame.R
+import be.bxl.formation.mynextgame.adapter.FavouriteGameAdapter
+import be.bxl.formation.mynextgame.adapter.GameItemDeco
+import be.bxl.formation.mynextgame.adapter.GameLastReleaseAdapter
 import be.bxl.formation.mynextgame.models.Game
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentWishList.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentWishList : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    var simpleList: ListView? = null
-    var countryList = arrayOf("India", "China", "australia", "Portugle", "America", "NewZealand")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
 
     }
 
@@ -43,38 +27,27 @@ class FragmentWishList : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view =  inflater?.inflate(R.layout.fragment_wish_list, container, false)
-        val listfavouriteview = view.findViewById<ListView>(R.id.searchView
-        )
+        val view = inflater.inflate(R.layout.fragment_wish_list, container, false)
+        val wlfavouriteView = view.findViewById<RecyclerView>(R.id.wish_list_vertical_recycler_view)
 
         // Inflate the layout for this fragment
         val gameDao = GameDao(requireContext())
 
         gameDao.openReadable()
-        val games: List<Any> = gameDao.all
+        val games: List<Game> = gameDao.all as List<Game>
+
+        println("----------------------------------------------------------$games----------------------------------------------------------")
+        val w1 = games.get(0)
+        println("---------- !!! we are looking for $w1")
+        /*
+        wlfavouriteView.adapter = FavouriteGameAdapter(R.layout.item_vertical_wish_list, games)
+        wlfavouriteView.addItemDecoration(GameItemDeco())
+        */
+
 
         gameDao.close()
         //val arrayAdapter: ArrayAdapter<Game> = ArrayAdapter<Game>(this, android.R.id.simpleListView, games)
         return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentWishList.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentWishList().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
